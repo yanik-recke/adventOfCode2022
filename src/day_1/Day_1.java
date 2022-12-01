9f79f0128e09b4b43d547f3f7ff88b2ccd95b524
yrec <yanik.recke@mailfence.com>
2022-12-01 07:06:55 +0100

    + solutions for day 1

---
+++ src/day_1/Day_1.java
@@ -0,0 +1,143 @@
+package day_1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Lösung für Tag 1.
+ * 
+ * @author Yanik Recke
+ *
+ */
+public class Day_1 {
+
+	public static void main(String[] args) {
+		String pathToInput = "src/day_1/input.txt";
+		
+		System.out.println(part1(pathToInput));
+		System.out.println(part2(pathToInput) + " - " + part2_secondSolution(pathToInput));
+	}
+	
+	
+	/**
+	 * Addiert die Werte bis zur nächsten Leerzeile auf
+	 * und vergleicht mit dem vorherigen größten Wert.
+	 * 
+	 * @param path - Pfad zum Puzzle Input
+	 * @return - Die größte Summe
+	 */
+	private static int part1(String path) {
+		int biggestSum = 0;
+		int sum = 0;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        if ("".equals(line)) {
+		        	if (biggestSum < sum) {
+		        		biggestSum = sum;
+		        	}
+		        	
+		        	sum = 0;
+		        } else {
+		        	sum += Integer.parseInt(line);
+		        }
+		        
+		        line = br.readLine();
+		    }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return biggestSum;
+	}
+	
+	
+	/**
+	 * Läuft den Input drei mal ab und findet jeweils
+	 * die größte Summe nach den vorherigen Durchläufen.
+	 * 
+	 * @param path - Pfad zum Puzzle Input
+	 * @return - Die Summe der drei größten Summen
+	 */
+	private static int part2(String path) {
+		List<Integer> biggestSums = new ArrayList<>();
+		int biggestSum;
+		int sum;
+		
+		while (biggestSums.size() < 3) {
+			sum = 0;
+			biggestSum = 0;
+			
+			try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+				String line = br.readLine();
+
+				while (line != null) {
+					if ("".equals(line)) {
+						if (biggestSum < sum && !biggestSums.contains(sum)) {
+							biggestSum = sum;
+						}
+		        	
+						sum = 0;
+					} else {
+						sum += Integer.parseInt(line);
+					}
+		        
+					line = br.readLine();
+				}
+		    
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+			
+			biggestSums.add(biggestSum);
+		}
+
+		
+		return biggestSums.get(0) + biggestSums.get(1) + biggestSums.get(2);
+	}
+	
+	
+	/**
+	 * Fügt jede Summe in eine Liste ein,
+	 * sortiert diese Liste absteigend und
+	 * addiert die ersten 3 Einträge auf.
+	 * 
+	 * @param path - Pfad zum Puzzle Input
+	 * @return - Die Summe der größten drei Summen
+	 */
+	private static int part2_secondSolution(String path) {
+		int sum = 0;
+		
+		List<Integer> list = new ArrayList<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        if ("".equals(line)) {
+		        	list.add(sum);
+		        	sum = 0;
+		        } else {
+		        	sum += Integer.parseInt(line);
+		        }
+		        
+		        line = br.readLine();
+		    }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		list.sort(Comparator.reverseOrder());
+		
+		return list.get(0) + list.get(1) + list.get(2);
+	}
+	
+}
---
+++ src/helpers/HelperMethods.java
@@ -0,0 +1,60 @@
+package helpers;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Methoden zum Einlesen des Inputs usw.
+ * 
+ * @author Yanik Recke
+ *
+ */
+public class HelperMethods {
+	
+	
+	/**
+	 * Liest einen einzeiligen Input in eine
+	 * Liste von Integern.
+	 * 
+	 * @param path - Pfad zur Datei
+	 * @return - Liste mit den Ganzzahlwerten
+	 */
+	public static List<Integer> getInputAsListOfIntegers(String path){
+		assert (path != null);
+		
+		List<Integer> input = new ArrayList<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        input.add(Integer.parseInt(line));
+		        line = br.readLine();
+		    }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return input;
+	}
+	
+	
+	
+	/**
+	 * Liest einen einzeiligen Input in einen
+	 * Array von Integern.
+	 * 
+	 * @param path - Pfad zur Datei
+	 * @return - Liste mit den Ganzzahlwerten
+	 */
+	public static int[] getInputAsArrayOfIntegers(String path){
+		assert (path != null);
+		
+		return getInputAsListOfIntegers(path).stream().mapToInt(i->i).toArray();
+	}
+
+}
