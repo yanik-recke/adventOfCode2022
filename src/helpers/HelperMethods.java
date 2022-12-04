ec10311cb510627e8565abb186788a908f3df40f
yrec <yanik.recke@mailfence.com>
2022-12-04 07:04:03 +0100

    + new helper

--- src/helpers/HelperMethods.java
+++ src/helpers/HelperMethods.java
@@ -72,6 +72,58 @@ public class HelperMethods {
 	
 	
 	/**
+	 * Liest einen Input in einen 
+	 * mehrdimensionalen Array von Integern.
+	 * 
+	 * @param path - Pfad zur Datei
+	 * @return - mehrdimensionaler Array an Integern
+	 */
+	public static int[][] getInputAsTwoDimensionalArray(String path) {
+		assert (path != null);
+
+		int[][] input;
+		int i = 0;
+		int j = 0;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		    String line = br.readLine();
+		    j = line.length();
+		    
+		    while (line != null) {
+		        i++;
+		        line = br.readLine();
+		    }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		input = new int[i][j];
+		
+		i = 0;
+		j = 0;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		    String line = br.readLine();
+		    
+		    while (line != null) {
+		    	for (int k = 0; k < line.length(); k++) {
+		    		input[i][k] = line.charAt(k) - '0';
+		    	}
+		    	
+		        i++;
+		        line = br.readLine();
+		    }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return input;
+	}
+	
+	
+	/**
 	 * Liest einen einzeiligen Input in einen
 	 * Array von Integern.
 	 * 
@@ -83,5 +135,20 @@ public class HelperMethods {
 		
 		return getInputAsListOfIntegers(path).stream().mapToInt(i->i).toArray();
 	}
+	
+	
+	/**
+	 * Prüft ob eine Position in den Grenzen eines 
+	 * Arrays liegt.
+	 * 
+	 * @param arr - der Array
+	 * @param x - x-Koordinate
+	 * @param y - y-Koordinate
+	 * @return - true, wenn Koordinaten innerhalb liegen, false wenn nicht
+	 */
+	public static boolean isInbounds(int[][] arr, int x, int y) {
+		return x >= 0 && y >= 0 && x < arr.length && y < arr[x].length;
+	}
+	
 
 }
