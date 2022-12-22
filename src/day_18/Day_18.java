ddf66525d200b483893edf2e21d76188ee4a39a1
yrec <yanik.recke@mailfence.com>
2022-12-22 22:14:50 +0100

    + solution for day 18

--- src/day_18/Day_18.java
+++ src/day_18/Day_18.java
@@ -1,8 +1,11 @@
 package day_18;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * Lösung für Tag 18.
@@ -13,8 +16,8 @@ public class Day_18 {
 
 	public static void main(String[] args) {
 		String pathToInput = "src/day_18/input.txt";
-		parse(pathToInput);
-		System.out.println(part1(pathToInput) + " - " + part2(pathToInput));
+		
+		System.out.println(part1(helpers.HelperMethods.getInputAsListOfString(pathToInput)) + " - " + part2(pathToInput));
 	}
 	
 	
@@ -27,8 +30,7 @@ public class Day_18 {
 	 * @param path - Pfad zum Puzzle Input
 	 * @return - Anzahl an freistehenden Flächen
 	 */
-	private static int part1(String path) {
-		List<String> input = helpers.HelperMethods.getInputAsListOfString(path);
+	private static int part1(List<String> input) {
 		
 		// Maximum finden
 		int max_x = Integer.MIN_VALUE;
@@ -52,7 +54,7 @@ public class Day_18 {
 		}
 		
 		// Koordinatensystem initialisieren und befüllen
-		int[][][] field = new int[max_x + 1][max_y + 1][max_z + 1];
+		int[][][] field = new int[max_x + 2][max_y + 2][max_z + 2];
 		
 		for (String line : input) {
 			String[] temp = line.split(",");
@@ -109,11 +111,15 @@ public class Day_18 {
 	
 	
 	/**
-	 * Zähler nur noch hochzählen, wenn in die gesamte Richtung
-	 * kein droplet gefunden wurde.
+	 * Von jeder Position aus prüfen, ob diese einen Weg
+	 * nach "draußen" hat, also von Luft berührt werden kann.
+	 * Die Wege speichern, damit man so wenig wie 
+	 * möglich doppelt prüft. Alle die keinen Weg nach "draußen" haben
+	 * werden dem Input hinzugefügt und dann wird part1() mit dem
+	 * neuen Input aufgerufen.
 	 * 
 	 * @param path - Pfad zum Puzzle Input
-	 * @return - Anzahl von nach außen zeigender Flächen
+	 * @return - Anzahl an Oberflächen, die von Luft berührt werden
 	 */
 	private static int part2(String path) {
 		List<String> input = helpers.HelperMethods.getInputAsListOfString(path);
@@ -139,6 +145,10 @@ public class Day_18 {
 			}
 		}
 		
+		System.out.println(max_x);
+		System.out.println(max_y);
+		System.out.println(max_z);
+		
 		// Koordinatensystem initialisieren und befüllen
 		int[][][] field = new int[max_x + 1][max_y + 1][max_z + 1];
 		
@@ -152,136 +162,187 @@ public class Day_18 {
 			field[x][y][z] = 1;
 		}
 		
-		int counter = 0;
-		
-		boolean[] dirs = new boolean[6];
-		
-		
+		Set<Position> all = new HashSet<>();
 		
 		for (int x = 0; x < field.length; x++) {
 			for (int y = 0; y < field[x].length; y++) {
 				for (int z = 0; z < field[x][y].length; z++) {
-					if (field[x][y][z] == 1) {
-						boolean isFacingOutwards = false;
-						// Prüfen in negative x-Richtung
-						boolean found = false;
-						for (int tempX = (x - 1); tempX >= 0 && !found; tempX--) {
-							if (field[tempX][y][z] == 1) {
-								found = true;
-							}
-						}
-						
-						if (!found || (x - 1) < 0) {
-							isFacingOutwards = true;
-							dirs[0] = true;
-						} else {
-							dirs[0] = false;
-						}
-		
 					
-						// Prüfen in negative y-Richtung
-						found = false;
-						for (int tempY = (y - 1); tempY >= 0 && !found; tempY--) {
-							if (field[x][tempY][z] == 1) {
-								found = true;
-							}
-						}
+					if (!all.contains(new Position(x, y, z))) {
+						List<Position> temp = canBeTouchedByAir(new Position(x, y, z), field, all);
 						
-						if (!found || (y - 1) < 0) {
-							isFacingOutwards = true;
-							dirs[1] = true;
-						} else {
-							dirs[1] = false;
-						}
-					
-						// Prüfen in negative z-Richtung
-						found = false;
-						for (int tempZ = (z - 1); tempZ >= 0 && !found; tempZ--) {
-							if (field[x][y][tempZ] == 1) {
-								found = true;
-							}
-						}
+						if (!temp.isEmpty()) {
+							for (Position pos : temp) {
+								input.add(pos.getX() + "," + pos.getY() + "," + pos.getZ());
+							} 
 						
-						if (!found || (z - 1) < 0) {
-							isFacingOutwards = true;
-							dirs[2] = true;
-						} else {
-							dirs[2] = false;
-						}
-					
-						// Prüfen in positive x-Richtung
-						found = false;
-						for (int tempX = (x + 1); tempX < field.length && !found; tempX++) {
-							if (field[tempX][y][z] == 1) {
-								found = true;
-							}
-						}
-						
-						if (!found || (x + 1) >= field.length) {
-							isFacingOutwards = true;
-							dirs[3] = true;
-						} else {
-							dirs[3] = false;
-						}
-					
-						// Prüfen in positive y-Richtung
-						found = false;
-						for (int tempY = (y + 1); tempY < field[x].length && !found; tempY++) {
-							if (field[x][tempY][z] == 1) {
-								found = true;
-							}
-						}
-						
-						if (!found || (y + 1) >= field[x].length) {
-							isFacingOutwards = true;
-							dirs[4] = true;
-						} else {
-							dirs[4] = false;
-						}
-					
-						// Prüfen in positive z-Richtung
-						found = false;
-						for (int tempZ = (z + 1); tempZ < field[x][y].length && !found; tempZ++) {
-							if (field[x][y][tempZ] == 1) {
-								found = true;
-							}
-						}
-						
-						if (!found || (z + 1) >= field[x][y].length) {
-							isFacingOutwards = true;
-							dirs[5] = true;
-						} else {
-							dirs[5] = false;
-						}
-						
-						if (isFacingOutwards) {
-							
+							all.addAll(temp);
 						}
 					}
 				}
 			}
 		}
+
 		
-		
-		// 4092 too high, 2038 too low, 2507
-		return counter;
+		return part1(input);
 	}
 	
 	
-	private static void parse(String path) {
-		List<String> input = helpers.HelperMethods.getInputAsListOfString(path);
-		int count = 0;
+	/**
+	 * Ermittelt, ob eine Position einen Weg nach
+	 * "draußen" hat.
+	 * 
+	 * @param pos - die Position
+	 * @param field - das aktuelle Feld
+	 * @param all - alle Positionen, die schon einen Weg nach draußen haben
+	 * @return - wenn kein Weg nach draußen gefunden wurde, eine leere Liste, sonst eine Liste,
+	 * 			 die einen Pfad nach draußen abbildet
+	 */
+	private static List<Position> canBeTouchedByAir(Position pos, int[][][] field, Set<Position> all) {
+		Set<Position> visited = new HashSet<>();
+		Queue<Position> toCheck = new LinkedList<>();
 		
-		String output = "{";
+		toCheck.add(pos);
+		visited.add(pos);
 		
-		for (String line : input) {
-			output = output.concat("(" + line.replace(',', '|') + ")");
-			count++;
+		while(!toCheck.isEmpty()) {
+			Position currPos = toCheck.poll();
+			if (all.contains(currPos)) {
+				return new ArrayList<>(visited);
+			}
+			
+			//System.out.println(currPos);
+			int x = currPos.getX();
+			int y = currPos.getY();
+			int z = currPos.getZ();
+			
+			if (field[x][y][z] != 1) {
+				// prüfen, ob außerhalb
+				boolean found = false;
+				boolean isInside = true;
+				boolean[] foundDir = new boolean[6];
+				
+				for (int tempX = (x - 1); tempX >= 0 && !found; tempX--) {
+					if (field[tempX][y][z] == 1) {
+						found = true;
+					}
+				}
+				
+				foundDir[0] = found;
+				
+				
+				found = false;
+				for (int tempY = (y - 1); tempY >= 0 && !found; tempY--) {
+					if (field[x][tempY][z] == 1) {
+						found = true;
+					}
+				}
+				
+				foundDir[1] = found;
+				
+
+				found = false;
+				for (int tempZ = (z - 1); tempZ >= 0 && !found; tempZ--) {
+					if (field[x][y][tempZ] == 1) {
+						found = true;
+					}
+				}
+
+				foundDir[2] = found;
+				
+
+				found = false;
+				for (int tempX = (x + 1); tempX < field.length && !found; tempX++) {
+					if (field[tempX][y][z] == 1) {
+						found = true;
+					}
+				}
+
+				foundDir[3] = found;
+				
+
+				found = false;
+				for (int tempY = (y + 1); tempY < field[x].length && !found; tempY++) {
+					if (field[x][tempY][z] == 1) {
+						found = true;
+					}
+				}
+
+				foundDir[4] = found;
+				
+
+				found = false;
+				for (int tempZ = (z + 1); tempZ < field[x][y].length && !found; tempZ++) {
+					if (field[x][y][tempZ] == 1) {
+						found = true;
+					}
+				}
+				
+				foundDir[5] = found;
+				
+				
+				for (boolean val : foundDir) {
+					if (!val) {
+						isInside = false;
+					}
+				}
+				
+				if (isInside) {
+					// Nachbar in negativ x-Richtung
+					if ((x - 1 >= 0) && !visited.contains(new Position(x - 1, y, z))) {
+						if (field[x - 1][y][z] == 0) {
+							toCheck.add(new Position(x - 1, y, z));
+							visited.add(currPos);
+						}
+					}
+				
+					// Nachbar in negativ y-Richtung
+					if (y - 1 >= 0 && !visited.contains(new Position(x, y - 1, z))) {
+						if (field[x][y - 1][z] == 0) {
+							toCheck.add(new Position(x, y - 1, z));
+							visited.add(currPos);
+						}
+					}
+				
+					// Nachbar in negativ z-Richtung
+					if (z - 1 >= 0 && !visited.contains(new Position(x, y, z - 1))) {
+						if (field[x][y][z - 1] == 0) {
+							toCheck.add(new Position(x, y, z - 1));
+							visited.add(currPos);
+						}
+					}
+				
+					// Nachbar in positiver x-Richtung
+					if (x + 1 < field.length && !visited.contains(new Position(x + 1, y, z))) {
+						if (field[x + 1][y][z] == 0) {
+							toCheck.add(new Position(x + 1, y, z));
+							visited.add(currPos);
+						}
+					}
+				
+					// Nachbar in positiver y-Richtung
+					if (y + 1 < field[x].length && !visited.contains(new Position(x, y + 1, z))) {
+						if (field[x][y + 1][z] == 0) {
+							toCheck.add(new Position(x, y + 1, z));
+							visited.add(currPos);
+						}
+					}
+				
+					// Nachbar in positiver z-Richtung
+					if (z + 1 < field[x][y].length && !visited.contains(new Position(x, y, z + 1))) {
+						if (field[x][y][z + 1] == 0) {
+							toCheck.add(new Position(x, y, z + 1));
+							visited.add(currPos);
+						}
+					}
+				} else {
+					return new ArrayList<>();
+				}
+			}
 		}
 		
-		output = output.concat("}");
 		
-		System.out.println(output);
+		return new ArrayList<>(visited);
 	}
 	
 }
