ebc07c9ce49e03e9285a117427f9121410eb5bbf
yanik-recke <yanik.recke@mailfence.com>
2022-12-27 21:57:13 +0100

    working on day 20

--- src/day_20/Day_20.java
+++ src/day_20/Day_20.java
@@ -2,6 +2,12 @@ package day_20;
 
 import java.util.List;
 
+/**
+ * Lösung für Tag 20.
+ * 
+ * @author Yanik Recke
+ *
+ */
 public class Day_20 {
 
 	public static void main(String[] args) {
@@ -11,18 +17,31 @@ public class Day_20 {
 	}
 	
 	
-	private static int part1(String path) {
-		ListNode head = new ListNode(0, null, null, true);
+	/**
+	 * Erstellen einer doppelt verketteten Listenstruktur.
+	 * Somit kann jede Nummer vorwärts und rückwärts 
+	 * bewegt werden. Jede Zahl bekommt eine ID, damit 
+	 * die Reihenfolge erhalten werden kann. Dann wird
+	 * jede Zahl durchgegangen und entsprechend verschoben.
+	 * 
+	 * @param path - Pfad zum Puzzle Input
+	 * @return - die Summe der 1000., 2000. und 3000. Zahl nach 0
+	 */
+	private static long part1(String path) {
+		ListNode head = new ListNode(0, null, null, true, 0);
+		int id = 1;
 		
 		List<String> input = helpers.HelperMethods.getInputAsListOfString(path);
 		
 		ListNode temp = head;
 		ListNode prev = null;
 		ListNode next = null;
+		int idOfZero = 0;
 		
 		for (int i = 0; i < input.size() - 1; i++) {
 			temp.setValue(Long.parseLong(input.get(i)));
-			next = new ListNode(0, null, null, false);
+			
+			next = new ListNode(0, null, null, false, id++);
 			temp.setNext(next);
 			next.setPrev(temp);
 			prev = temp;
@@ -34,9 +53,64 @@ public class Day_20 {
 		temp.setNext(head);
 		head.setPrev(temp);
 		
+		boolean found = false;
+		ListNode runner = head;
 		
 		
-		return 0;
+		// Id von 0 finden
+		while (!found) {
+			if (runner.getValue() == 0) {
+				idOfZero = runner.getId();
+				found = true;
+			}
+			
+			runner = runner.getNext();
+		}
+		
+		for (int i = 0; i < id; i++) {
+			ListNode currNode = head.getNodeByIdForward(i);
+			long move = currNode.getValue();
+			
+			ListNode insertAt = currNode;
+			
+			if (move != 0) {
+				currNode.removeNode();
+			}
+			
+			if (move > 0) {
+				for (int j = 0; j < move; j++) {
+					insertAt = insertAt.getNext();
+				}
+			} else if (move < 0) {
+				for (int j = 0; j > move; j--) {
+					insertAt = insertAt.getPrev();
+				}
+				insertAt = insertAt.getPrev();
+			} else {
+				// wenn 0, nichts machen
+				continue;
+			}
+			
+			insertAt.insertNode(currNode);
+			head = insertAt;
+		}
+		
+		ListNode currNode = head.getNodeByIdForward(idOfZero);
+		
+		long sum = 0;
+		for (int i = 0; i <= 3000; i++) {
+			if (i == 1000) {
+				sum += currNode.getValue();
+			} else if (i == 2000) {
+				sum += currNode.getValue();
+			} else if (i == 3000) {
+				sum += currNode.getValue();
+			}
+			
+			currNode = currNode.getNext();
+		}
+		
+		return sum;
 	}
 	
 	
--- src/day_20/ListNode.java
+++ src/day_20/ListNode.java
@@ -1,43 +1,176 @@
 package day_20;
 
+
+/**
+ * Implementierung einer doppelt
+ * verketten Liste.
+ * 
+ * @author Yanik Recke
+ */
 public class ListNode {
+	/** Zahl des Elements */
 	private long value;
+	/** Id des Elements */
+	private final int id;
+	/** Nächstes Element der Liste */
 	private ListNode next;
+	/** Vorheriges Element der Liste */
 	private ListNode prev;
-	
+	/** Ob das Element der ursprüngliche Head ist */
 	public final boolean isHead;
 	
 	
-	public ListNode(long value, ListNode next, ListNode prev, boolean isHead) {
+	/**
+	 * Konstruktor.
+	 * 
+	 * @param value - Zahl
+	 * @param next - nächstes Element
+	 * @param prev - vorheriges Element
+	 * @param isHead - ob das Element der Kopf ist
+	 * @param id - die Id des Elements
+	 */
+	public ListNode(long value, ListNode next, ListNode prev, boolean isHead, int id) {
 		this.value = value;
 		this.next = next;
 		this.prev = prev;
 		this.isHead = isHead;
+		this.id = id;
 	}
 	
 	
+	/**
+	 * Findet das Element der Liste
+	 * mit der entsprechenden Id.
+	 * 
+	 * @param id - die Id des zu findenden Elements
+	 * @return - das entsprechende Element
+	 */
+	public ListNode getNodeByIdForward(int id) {
+		if (this.id == id) {
+			return this;
+		} else {
+			return this.next.getNodeByIdForward(id);
+		}
+	}
+	
+	
+	/**
+	 * Entfernt ein Element
+	 * aus der Liste.
+	 * 
+	 * @return - das entfernte Element
+	 */
+	public ListNode removeNode() {
+		this.prev.setNext(this.next);
+		this.next.setPrev(this.prev);
+		
+		return this;
+	}
+	
+	
+	/**
+	 * Fügt ein Element in die
+	 * Liste ein.
+	 * 
+	 * @param node - das einzufügende Element
+	 */
+	public void insertNode(ListNode node) {
+		/*
+		 * A, B, C, D
+		 * -> remove D
+		 * A, B, C
+		 * -> insert D after B
+		 * A, B, D, C
+		 * -> B.next = D, D.prev = B, D.next = C, C.prev = D
+		 * B = this, D = node, C = this.prev
+		 */
+		this.next.prev = node;
+		node.next = this.next;
+		node.prev = this;
+		
+		this.next = node;
+	}
+	
+	
+	/**
+	 * Getter für die Zahl
+	 * des Elements.
+	 * 
+	 * @return - die Zahl
+	 */
 	public long getValue() {
 		return this.value;
 	}
 	
+	
+	/**
+	 * Getter für das nächste Element
+	 * der Liste.
+	 * 
+	 * @return - das nächste Element
+	 */
 	public ListNode getNext() {
 		return this.next;
 	}
 	
+	
+	/**
+	 * Getter für das vorherige
+	 * Element der Liste.
+	 * 
+	 * @return - das vorherige Element
+	 */
 	public ListNode getPrev() {
 		return this.prev;
 	}
 	
+	
+	/**
+	 * Getter für die Id des
+	 * Elements.
+	 * 
+	 * @return - die Id
+	 */
+	public int getId() {
+		return this.id;
+	}
+	
+	
+	/**
+	 * Setter für die Zahl
+	 * des Elements.
+	 * 
+	 * @param value - die Zahl
+	 */
 	public void setValue(long value) {
 		this.value = value;
 	}
 	
+	
+	/**
+	 * Setter für das nächste
+	 * Element der Liste.
+	 * 
+	 * @param next - das neue nächste Element der Liste
+	 */
 	public void setNext(ListNode next) {
 		this.next = next;
 	}
 	
+	
+	/**
+	 * Setter für das vorherige Element
+	 * der Liste.
+	 * 
+	 * @param prev - das neue vorherige Element der Liste
+	 */
 	public void setPrev(ListNode prev) {
 		this.prev = prev;
+	}
+	
+	@Override
+	public String toString() {
+		return "value: " + this.value;
 	}
 	
 }
