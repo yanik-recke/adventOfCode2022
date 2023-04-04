e6140abf7130ed28e92873144c0f7c2ccd7024f6
Yanik Luis Recke <yr0082@ham.ituv-software.de>
2023-04-04 14:17:16 +0200

    working on day 24

---
+++ src/day_24/Day_24.java
@@ -0,0 +1,25 @@
+package day_24;
+
+/**
+ * Lösungen für Tag 24.
+ *
+ * @author Yanik Recke
+ */
+public class Day_24 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static long part1(String pathToInput) {
+
+        return 0;
+    }
+
+
+    private static long part2(String pathToInput) {
+
+        return 0;
+    }
+
+}
