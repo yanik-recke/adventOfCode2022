ccf539fa6d72661f718771bb64ca6dd17a6728f0
Yanik Recke <recke.yanik@outlook.de>
2026-09-21 22:20:14 +0200

    Minor changes

--- python/25.py
+++ python/25.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from z3 import Solver, Int, Or, sat
 
 # from decimal to quinary
-def g(s: str) -> int:
+def g(s: str) -> str:
     n = int(s)
     remainders = []
 
@@ -10,7 +10,7 @@ def g(s: str) -> int:
         remainders.append(str(n % 5))
         n = n // 5
 
-    return int("".join(remainders[::-1]))
+    return "".join(remainders[::-1])
 
 safu = {
     "2": 2,
@@ -23,24 +23,24 @@ safu = {
 rev_safu = {v: k for k, v in safu.items()}
 
 def from_safu(s: str) -> int:
-    return sum(safu[c] * pow(5, i) for i, c in enumerate(s[::-1]))
+    return sum(safu[c] * (5 ** i) for i, c in enumerate(s[::-1]))
 
 dec_n = sum(from_safu(l) for l in Path("in25.txt").read_text().splitlines())
 qui_n = g(str(dec_n))
 
 s = Solver()
 
-x = [Int(f'{i}') for i, _ in enumerate(str(qui_n))]
+x = [Int(f'x{i}') for i in range(len(qui_n))]
 
 # x can only be one of these
-for i, _ in enumerate(str(qui_n)):
+for i in range(len(qui_n)):
     s.add(Or(x[i] == 2, x[i] == 1, x[i] == 0, x[i] == -1, x[i] == -2))
 
-s.add(sum(x[i] * pow(5, i) for i, _ in enumerate(str(qui_n))) == dec_n)
+s.add(sum(x[i] * pow(5, i) for i in range(len(qui_n))) == dec_n)
 
 if s.check() == sat:
     model = s.model()
-    result = "".join(rev_safu[model[x[i]].as_long()] for i in range(len(str(qui_n)) - 1, -1, -1))
+    result = "".join(rev_safu[model[x[i]].as_long()] for i in range(len(qui_n) - 1, -1, -1))
     print(result)
 else:
     print("No model found")
